ff7b79d2f7adb3f82696cac066e89a03c43d26f7
kledkaew <kledkaewmimi@gmail.com>
2026-07-10 19:24:18 +0700

    Fix bee heart active render scale

--- app.js
+++ app.js
@@ -2419,6 +2419,7 @@ function createBraceletComponentList() {
           renderWidthMm: 12.5,
           renderHeightMm: 18,
           ...renderTuning,
+          visualScale: 0.58,
           uniqueId: item.uniqueId
         };
       }
